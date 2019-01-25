2ccb96f72cae226ed710f1415bb8a7cb5b1d1c76
prmsantos1112 <prmsantosster@gmail.com>
2019-01-24 23:08:18 -0200

    Default Methods - Afther Implementation - Java

---
+++ src/Application/ProgramDefaultMethod.java
@@ -0,0 +1,31 @@
+package Application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import services.BrazilInterestService;
+
+public class ProgramDefaultMethod {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Amount: ");
+		double amount = sc.nextDouble();
+		System.out.print("Months: ");
+		int months = sc.nextInt();
+		
+		BrazilInterestService interEst = new BrazilInterestService(2.0);
+		double payment = interEst.payment(amount, months);
+		
+		System.out.println("Payment After " + months + "months: ");
+		System.out.println(String.format("%.2f", payment));		
+		
+		sc.close();
+
+	}
+
+}
